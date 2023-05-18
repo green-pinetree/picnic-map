9cb073fc24449eedf9a47d2c8f335a0ec982a70f
sunmin <polarlsm1231@gmail.com>
2023-05-18 14:33:29 +0900

    feat: favicon 적용 #5

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="og:site_name" content="picnic-map" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="picnic-map" />
+        <meta charSet="UTF-8" />
+        <link rel="icon" href="/favicon.svg" />
+      </Head>
       <body>
         <Main />
         <NextScript />
