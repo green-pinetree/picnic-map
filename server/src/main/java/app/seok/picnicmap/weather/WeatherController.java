be6b239d6ee2909202dc81ae6370eb3333d00df3
Yongseok <yongseok.dev@gmail.com>
2023-05-19 17:56:21 +0900

    feat: 날씨조회 API 세팅 #22

--- server/src/main/java/app/seok/picnicmap/weather/WeatherController.java
+++ server/src/main/java/app/seok/picnicmap/weather/WeatherController.java
@@ -1,18 +1,21 @@
 package app.seok.picnicmap.weather;
 
-import app.seok.picnicmap.location.Location;
+import app.seok.picnicmap.api.openapi.OpenapiApiExplorer;
+import app.seok.picnicmap.location.LocationDTO;
 import app.seok.picnicmap.location.LocationService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
 
 @Controller
 @RequiredArgsConstructor
 public class WeatherController {
     private final LocationService locationService;
+    private final OpenapiApiExplorer openapiApiExplorer;
 
     @GetMapping("/api/weather")
     @ResponseBody
@@ -33,10 +36,18 @@ public class WeatherController {
         System.out.println(weatherRequestDTO.toString());
         Double lng = Double.parseDouble(weatherRequestDTO.getLng());
         Double lat = Double.parseDouble(weatherRequestDTO.getLat());
-        Location location = locationService.findNearestLocation(lat, lng);
+        LocationDTO location = locationService.findNearestLocation(lat, lng);
         System.out.println(location);
         return location.toString();
     }
 
+    @GetMapping("/api/weather/dan")
+    @ResponseBody
+    public String vilageFcst(WeatherRequestDTO weatherRequestDTO) throws IOException {
+        System.out.println(weatherRequestDTO.toString());
+        Double lng = Double.parseDouble(weatherRequestDTO.getLng());
+        Double lat = Double.parseDouble(weatherRequestDTO.getLat());
+        return openapiApiExplorer.VilageFcstInfoAPI(lng, lat);
+    }
 
 }
