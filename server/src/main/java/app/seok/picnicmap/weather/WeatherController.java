cf9a027f273af9d2c8b4f2b7ddb6e559da6d509b
yongseok <yongseok.dev@gmail.com>
2023-05-20 19:40:47 +0900

    feat: 기상현황 API 호출 #22

--- server/src/main/java/app/seok/picnicmap/weather/WeatherController.java
+++ server/src/main/java/app/seok/picnicmap/weather/WeatherController.java
@@ -1,38 +1,39 @@
 package app.seok.picnicmap.weather;
 
-import app.seok.picnicmap.api.openapi.OpenapiApiExplorer;
+import app.seok.picnicmap.api.openapi.MidLandFcstDTO;
+import app.seok.picnicmap.api.openapi.UltraSrtNcstDTO;
+import app.seok.picnicmap.api.openapi.VilageFcstDTO;
+import app.seok.picnicmap.api.seoul.RealtimeCityAirDTO;
 import app.seok.picnicmap.location.LocationDTO;
 import app.seok.picnicmap.location.LocationService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class WeatherController {
     private final LocationService locationService;
-    private final OpenapiApiExplorer openapiApiExplorer;
+    private final WeatherService weatherService;
 
     @GetMapping("/api/weather")
-    @ResponseBody
     public String weather(WeatherRequestDTO weatherRequestDTO) {
         return weatherRequestDTO.toString();
     }
 
     @GetMapping("/api/weather/input")
-    @ResponseBody
     public String locationInput(WeatherRequestDTO weatherRequestDTO) throws JsonProcessingException {
         locationService.saveLocation();
         return weatherRequestDTO.toString();
     }
 
     @GetMapping("/api/weather/location")
-    @ResponseBody
-    public String location(WeatherRequestDTO weatherRequestDTO) throws JsonProcessingException {
+    public String getLocation(WeatherRequestDTO weatherRequestDTO) throws JsonProcessingException {
         System.out.println(weatherRequestDTO.toString());
         Double lng = Double.parseDouble(weatherRequestDTO.getLng());
         Double lat = Double.parseDouble(weatherRequestDTO.getLat());
@@ -41,13 +42,25 @@ public class WeatherController {
         return location.toString();
     }
 
-    @GetMapping("/api/weather/dan")
-    @ResponseBody
-    public String vilageFcst(WeatherRequestDTO weatherRequestDTO) throws IOException {
-        System.out.println(weatherRequestDTO.toString());
-        Double lng = Double.parseDouble(weatherRequestDTO.getLng());
-        Double lat = Double.parseDouble(weatherRequestDTO.getLat());
-        return openapiApiExplorer.VilageFcstInfoAPI(lng, lat);
+    @GetMapping("/test/weather/ultra")
+    public UltraSrtNcstDTO testUltraSrtNcst() throws IOException {
+        return weatherService.getUltraSrtNcst();
     }
 
+    @GetMapping("/test/weather/vilage")
+    public VilageFcstDTO testVilageFcst() throws IOException {
+        return weatherService.getVilageFcst();
+    }
+
+    @GetMapping("/test/weather/mid")
+    public MidLandFcstDTO testMidLandFcst() throws IOException {
+        LocalDateTime now = LocalDateTime.now();
+        String date = now.format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+        return weatherService.getMidLandFcst(date);
+    }
+
+    @GetMapping("/test/weather/air")
+    public RealtimeCityAirDTO getAir() throws IOException {
+        return weatherService.getRealtimeCityAir();
+    }
 }
--- server/src/main/java/app/seok/picnicmap/weather/WeatherService.java
+++ server/src/main/java/app/seok/picnicmap/weather/WeatherService.java
@@ -1,22 +1,75 @@
 package app.seok.picnicmap.weather;
 
+import app.seok.picnicmap.api.openapi.MidLandFcstDTO;
+import app.seok.picnicmap.api.openapi.OpenapiApiExplorer;
+import app.seok.picnicmap.api.openapi.UltraSrtNcstDTO;
+import app.seok.picnicmap.api.openapi.VilageFcstDTO;
+import app.seok.picnicmap.api.seoul.RealtimeCityAirDTO;
+import app.seok.picnicmap.api.seoul.SeoulApiExplorer;
+import app.seok.picnicmap.util.ApiTimeCalculator;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Service
+@RequiredArgsConstructor
 public class WeatherService {
+    private final ObjectMapper objectMapper;
+    private final OpenapiApiExplorer openapiApiExplorer;
+    private final SeoulApiExplorer seoulApiExplorer;
 
     /*
      * 지역확인
      * */
 
     /*
-    * 기상청 단기예보
+     * 기상청 초단기 현황
      * */
+    public UltraSrtNcstDTO getUltraSrtNcst() throws IOException {
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime previous = now.minusHours(1);
+        String time = previous.format(DateTimeFormatter.ofPattern("HH00"));
+        String date = previous.format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+        String json = openapiApiExplorer.apiUltraSrtNcst(date, time, 55, 127);
+        UltraSrtNcstDTO dto = objectMapper.readValue(json, UltraSrtNcstDTO.class);
+        return dto;
+    }
 
+    /*
+     * 기상청 단기예보
+     * */
+    public VilageFcstDTO getVilageFcst() throws IOException {
+        LocalDateTime now = LocalDateTime.now();
+        String time = ApiTimeCalculator.apiVilageBaseTime();
+        String date = now.format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+        if (Integer.parseInt(now.format(DateTimeFormatter.ofPattern("HH00"))) > Integer.parseInt(time)) {
+            LocalDateTime previousDate = now.minusDays(1);
+            date = previousDate.format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+        }
+        String json = openapiApiExplorer.apiVilageFcst(date, time, 55, 127);
+        VilageFcstDTO dto = objectMapper.readValue(json, VilageFcstDTO.class);
+        return dto;
+    }
 
     /*
      * 기상청 중기예보
      * */
-
+    public MidLandFcstDTO getMidLandFcst(String date) throws IOException {
+        String json = openapiApiExplorer.apiMidFcst(date);
+        MidLandFcstDTO dto = objectMapper.readValue(json, MidLandFcstDTO.class);
+        return dto;
+    }
 
     /*
      * 대기정보
      * */
+    public RealtimeCityAirDTO getRealtimeCityAir() throws IOException {
+        String json = seoulApiExplorer.API("RealtimeCityAir", 1, 1000);
+        RealtimeCityAirDTO dto = objectMapper.readValue(json, RealtimeCityAirDTO.class);
+        return dto;
+    }
 }
