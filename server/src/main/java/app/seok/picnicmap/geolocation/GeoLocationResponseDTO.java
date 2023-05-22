08fdcd73b2828603fc457e31c5dd01e59ad986e7
yongseok <yongseok.dev@gmail.com>
2023-05-22 23:41:11 +0900

    fix: 위치 조회 API 수정 ##33

--- server/src/main/java/app/seok/picnicmap/geolocation/GeoLocationResponseDTO.java
+++ server/src/main/java/app/seok/picnicmap/geolocation/GeoLocationResponseDTO.java
@@ -1,43 +1,48 @@
 package app.seok.picnicmap.geolocation;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.util.List;
 
 @Setter
 @Getter
 public class GeoLocationResponseDTO {
-    private Integer status;
-    private String massage;
-    private Position data;
 
-    @Setter
-    @Getter
-    public static class Position {
-        private double lng;
-        private double lat;
-    }
+  private Integer status;
+  private String massage;
+  private Position data;
+  @JsonIgnore
+  private String returnMessage;
 
-    public static GeoLocationResponseDTO OkFromGeoLocation(GeoLocation dbResult) {
-        double lng = dbResult.getLng();
-        double lat = dbResult.getLat();
-        GeoLocationResponseDTO geoLocationResponse = new GeoLocationResponseDTO();
-        geoLocationResponse.setStatus(200);
-        geoLocationResponse.setMassage("Success");
-        Position position = new Position();
-        position.setLng(lng);
-        position.setLat(lat);
-        geoLocationResponse.setData(position);
-        return geoLocationResponse;
-    }
-    public static GeoLocationResponseDTO Ok() {
-        GeoLocationResponseDTO geoLocationResponse = new GeoLocationResponseDTO();
-        geoLocationResponse.setStatus(200);
-        Position position = new Position();
-        position.setLng(126.977380);
-        position.setLat(37.575843);
-        geoLocationResponse.setData(position);
-        return geoLocationResponse;
-    }
+  @Setter
+  @Getter
+  public static class Position {
+
+    private double lng;
+    private double lat;
+  }
+
+  public static GeoLocationResponseDTO OkFromGeoLocation(GeoLocation dbResult) {
+    double lng = dbResult.getLng();
+    double lat = dbResult.getLat();
+    GeoLocationResponseDTO geoLocationResponse = new GeoLocationResponseDTO();
+    geoLocationResponse.setStatus(200);
+    geoLocationResponse.setMassage("Success " + dbResult.getR2());
+    Position position = new Position();
+    position.setLng(lng);
+    position.setLat(lat);
+    geoLocationResponse.setData(position);
+    return geoLocationResponse;
+  }
+
+  public static GeoLocationResponseDTO Ok() {
+    GeoLocationResponseDTO geoLocationResponse = new GeoLocationResponseDTO();
+    geoLocationResponse.setMassage("위치를 확인해 주세요.");
+    geoLocationResponse.setStatus(200);
+    Position position = new Position();
+    position.setLng(126.977380);
+    position.setLat(37.575843);
+    geoLocationResponse.setData(position);
+    return geoLocationResponse;
+  }
 }
--- server/src/main/java/app/seok/picnicmap/geolocation/GeoLocationService.java
+++ server/src/main/java/app/seok/picnicmap/geolocation/GeoLocationService.java
@@ -3,49 +3,51 @@ package app.seok.picnicmap.geolocation;
 import app.seok.picnicmap.api.naver.NcloudApiExplorer;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
 import java.time.LocalDateTime;
 import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class GeoLocationService {
-    private final NcloudApiExplorer ncloudApiExplorer;
-    private final GeoLocationRepository geoLocationRepository;
 
-    public GeoLocationResponseDTO getGeoLocation(String ipAddress) {
-        //DB에서 조회 최근 1시간 이내 조회 했던 IP는 해당 값으로 출력
-        LocalDateTime startTime = LocalDateTime.now().minusHours(1);
-        Optional<GeoLocation> optionalGeoLocation = geoLocationRepository.findRecentRecordByIpAddress(ipAddress, startTime);
+  private final NcloudApiExplorer ncloudApiExplorer;
+  private final GeoLocationRepository geoLocationRepository;
 
-        GeoLocation geoLocation;
-        if (optionalGeoLocation.isPresent()) {
-            geoLocation = optionalGeoLocation.get();
-        } else {
-            try {
-                geoLocation = callNcloudApiGeolocation(ipAddress);
-                geoLocationRepository.save(geoLocation);
-            } catch (JsonProcessingException e) {
-                geoLocation = new GeoLocation();
-                throw new RuntimeException(e);
-            }
-        }
+  public GeoLocationResponseDTO getGeoLocation(String ipAddress) {
+    //DB에서 조회 최근 1시간 이내 조회 했던 IP는 해당 값으로 출력
+    LocalDateTime startTime = LocalDateTime.now().minusHours(1);
+    Optional<GeoLocation> optionalGeoLocation = geoLocationRepository.findRecentRecordByIpAddress(
+        ipAddress, startTime);
 
-        if ("서울특별시".equals(geoLocation.getR1())) {
-            return GeoLocationResponseDTO.OkFromGeoLocation(geoLocation);
-        }
-        return GeoLocationResponseDTO.Ok();
+    GeoLocation geoLocation;
+    if (optionalGeoLocation.isPresent()) {
+      geoLocation = optionalGeoLocation.get();
+    } else {
+      try {
+        geoLocation = callNcloudApiGeolocation(ipAddress);
+        geoLocationRepository.save(geoLocation);
+      } catch (JsonProcessingException e) {
+        geoLocation = new GeoLocation();
+        throw new RuntimeException(e);
+      }
     }
 
-    private GeoLocation callNcloudApiGeolocation(String ipAddress) throws JsonProcessingException {
-        String jsonString = String.valueOf(ncloudApiExplorer.getGeolocation(ipAddress));
-        ObjectMapper objectMapper = new ObjectMapper();
-        jsonString = jsonString.replace("\"long\":", "\"lng\":");
-        GeoLocationDTO responseDTO = objectMapper.readValue(jsonString, GeoLocationDTO.class);
-        GeoLocation geoLocation = responseDTO.getGeoLocation();
-        geoLocation.setIpAddress(ipAddress);
-        return geoLocation;
+    if ("서울특별시".equals(geoLocation.getR1())) {
+      return GeoLocationResponseDTO.OkFromGeoLocation(geoLocation);
     }
+
+    return GeoLocationResponseDTO.Ok();
+  }
+
+  private GeoLocation callNcloudApiGeolocation(String ipAddress) throws JsonProcessingException {
+    String jsonString = String.valueOf(ncloudApiExplorer.getGeolocation(ipAddress));
+    ObjectMapper objectMapper = new ObjectMapper();
+    jsonString = jsonString.replace("\"long\":", "\"lng\":");
+    GeoLocationDTO responseDTO = objectMapper.readValue(jsonString, GeoLocationDTO.class);
+    GeoLocation geoLocation = responseDTO.getGeoLocation();
+    geoLocation.setIpAddress(ipAddress);
+    return geoLocation;
+  }
 }
