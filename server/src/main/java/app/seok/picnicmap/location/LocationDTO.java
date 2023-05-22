3175f757b8d67c9dc7b1aedc41acc289e0340107
Yongseok <yongseok.dev@gmail.com>
2023-05-22 14:44:09 +0900

    fix: 위치정보 추가 #22

--- server/src/main/java/app/seok/picnicmap/location/LocationDTO.java
+++ server/src/main/java/app/seok/picnicmap/location/LocationDTO.java
@@ -4,8 +4,12 @@ import lombok.Data;
 
 @Data
 public class LocationDTO {
-    private String district;
-    private Double lat;
-    private Double lng;
-    private Integer code;
+
+  private String district;
+  private Double lat;
+  private Double lng;
+  private Integer code;
+  private String r2;
+  private Integer x;
+  private Integer y;
 }
--- server/src/main/java/app/seok/picnicmap/location/LocationService.java
+++ server/src/main/java/app/seok/picnicmap/location/LocationService.java
@@ -2,12 +2,7 @@ package app.seok.picnicmap.location;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-import javax.servlet.http.HttpServletRequest;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -15,68 +10,79 @@ import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class LocationService {
-    private final LocationRepository locationRepository;
-    private final HttpServletRequest request;
 
-    public void saveLocation() throws JsonProcessingException {
-        try {
-            String url = "http://"+request.getServerName()+":"+request.getServerPort()+"/json/district.json";
-            URL jsonUrl = new URL(url);
-            BufferedReader reader = new BufferedReader(new InputStreamReader(jsonUrl.openStream(), StandardCharsets.UTF_8));
-            StringBuilder jsonData = new StringBuilder();
-            String line;
-            while ((line = reader.readLine()) != null) {
-                jsonData.append(line);
-            }
-            reader.close();
-            ObjectMapper objectMapper = new ObjectMapper();
-            Map<String, Object> districtsMap = objectMapper.readValue(jsonData.toString(), new TypeReference<Map<String, Object>>() {
-            });
-            List<Map<String, Object>> districts = (List<Map<String, Object>>) districtsMap.get("district");
+  private final LocationRepository locationRepository;
+  private final HttpServletRequest request;
 
-            for (Map<String, Object> district : districts) {
-                Location location = new Location();
-                Integer code = (Integer) district.get("code");
-                location.setCode(code);
-                String r1 = (String) district.get("r1");
-                location.setR1(r1);
-                String r2 = (String) district.get("r2");
-                location.setR2(r2);
-                String r3 = (String) district.get("r3");
-                location.setR3(r3);
-                Double lng = (Double) district.get("lng");
-                location.setLng(lng);
-                Double lat = (Double) district.get("lat");
-                location.setLat(lat);
-                Integer x = (Integer) district.get("x");
-                location.setX(x);
-                Integer y = (Integer) district.get("y");
-                location.setY(y);
-                locationRepository.save(location);
+  public void saveLocation() throws JsonProcessingException {
+    try {
+      String url = "http://" + request.getServerName() + ":" + request.getServerPort()
+          + "/json/district.json";
+      URL jsonUrl = new URL(url);
+      BufferedReader reader = new BufferedReader(
+          new InputStreamReader(jsonUrl.openStream(), StandardCharsets.UTF_8));
+      StringBuilder jsonData = new StringBuilder();
+      String line;
+      while ((line = reader.readLine()) != null) {
+        jsonData.append(line);
+      }
+      reader.close();
+      ObjectMapper objectMapper = new ObjectMapper();
+      Map<String, Object> districtsMap = objectMapper.readValue(jsonData.toString(),
+          new TypeReference<Map<String, Object>>() {
+          });
+      List<Map<String, Object>> districts = (List<Map<String, Object>>) districtsMap.get(
+          "district");
+
+      for (Map<String, Object> district : districts) {
+        Location location = new Location();
+        Integer code = (Integer) district.get("code");
+        location.setCode(code);
+        String r1 = (String) district.get("r1");
+        location.setR1(r1);
+        String r2 = (String) district.get("r2");
+        location.setR2(r2);
+        String r3 = (String) district.get("r3");
+        location.setR3(r3);
+        Double lng = (Double) district.get("lng");
+        location.setLng(lng);
+        Double lat = (Double) district.get("lat");
+        location.setLat(lat);
+        Integer x = (Integer) district.get("x");
+        location.setX(x);
+        Integer y = (Integer) district.get("y");
+        location.setY(y);
+        locationRepository.save(location);
 //                System.out.println(location);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+  }
 
-    /*
-     * 지역확인
-     * */
-    public LocationDTO findNearestLocation(Double targetLatitude, Double targetLongitude) {
-        return locationToLocationDTO(locationRepository.findNearestLocation(targetLatitude, targetLongitude));
-    }
+  /*
+   * 지역확인
+   * */
+  public LocationDTO findNearestLocation(Double targetLatitude, Double targetLongitude) {
+    return locationToLocationDTO(
+        locationRepository.findNearestLocation(targetLatitude, targetLongitude));
+  }
 
-    public LocationDTO locationToLocationDTO(Location location){
-        LocationDTO locationDTO = new LocationDTO();
-        locationDTO.setCode(location.getCode());
-        locationDTO.setDistrict(location.getR2());
-        locationDTO.setLng(location.getLng());
-        locationDTO.setLat(location.getLat());
-        return locationDTO;
-    }
+  public LocationDTO locationToLocationDTO(Location location) {
+    LocationDTO locationDTO = new LocationDTO();
+    locationDTO.setCode(location.getCode());
+    locationDTO.setR2(location.getR2());
+    locationDTO.setX(location.getX());
+    locationDTO.setY(location.getY());
+    locationDTO.setLng(location.getLng());
+    locationDTO.setLat(location.getLat());
+    return locationDTO;
+  }
 }
