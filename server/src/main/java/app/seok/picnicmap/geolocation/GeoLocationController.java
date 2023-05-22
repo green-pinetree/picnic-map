486c55412a4790d21b84e92d7beec47264f5cff5
Yongseok <yongseok.dev@gmail.com>
2023-05-22 14:43:00 +0900

    refactor: 위치정보 #22

--- server/src/main/java/app/seok/picnicmap/geolocation/GeoLocationController.java
+++ server/src/main/java/app/seok/picnicmap/geolocation/GeoLocationController.java
@@ -1,26 +1,24 @@
 package app.seok.picnicmap.geolocation;
 
-import app.seok.picnicmap.api.naver.NcloudApiExplorer;
+import javax.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequiredArgsConstructor
 public class GeoLocationController {
-    private final GeoLocationService geoLocationService;
 
-    @GetMapping("/api/geolocation")
-    public GeoLocationResponseDTO getLocation(@RequestHeader(value = "X-Forwarded-For", required = false) String ipAddress, HttpServletRequest request) {
-        if (ipAddress == null || ipAddress.isEmpty()) {
-            ipAddress = request.getRemoteAddr();
-        }
-        System.out.println(ipAddress);
-        return geoLocationService.getGeoLocation(ipAddress);
+  private final GeoLocationService geoLocationService;
+
+  @GetMapping("/api/geolocation")
+  public GeoLocationResponseDTO getLocation(
+      @RequestHeader(value = "X-Forwarded-For", required = false) String ipAddress,
+      HttpServletRequest request) {
+    if (ipAddress == null || ipAddress.isEmpty()) {
+      ipAddress = request.getRemoteAddr();
     }
+    return geoLocationService.getGeoLocation(ipAddress);
+  }
 }
