f2c0e930487f0f7982324964e570a515e90d0d2a
sunmin <polarlsm1231@gmail.com>
2023-05-26 19:31:51 +0900

    fix: 사용자가 위치 정보 허용 안했을 때 예외 처리

--- client/src/hooks/useUserLocation.ts
+++ client/src/hooks/useUserLocation.ts
@@ -6,14 +6,21 @@ import { addLocation } from '@/store/userLocation';
 export const useUserLocation = () => {
   const [isGetLocation, setIsGetLocation] = useState(false);
   const dispatch = useDispatch();
+  const setUserLocation = (position: { coords: { latitude: number; longitude: number } }) => {
+    const location = { latitude: position.coords.latitude, longitude: position.coords.longitude };
+    dispatch(addLocation({ ...location }));
+    dispatch(addCenter({ ...location }));
+    setIsGetLocation(false);
+  };
+  const setExceptLocation = () => {
+    const location = { latitude: 37.574187, longitude: 126.976882 };
+    dispatch(addLocation({ ...location }));
+    dispatch(addCenter({ ...location }));
+    setIsGetLocation(false);
+  };
   useEffect(() => {
     setIsGetLocation(true);
-    navigator.geolocation.getCurrentPosition((position) => {
-      const location = { latitude: position.coords.latitude, longitude: position.coords.longitude };
-      dispatch(addLocation({ ...location }));
-      dispatch(addCenter({ ...location }));
-      setIsGetLocation(false);
-    });
+    navigator.geolocation.getCurrentPosition(setUserLocation, setExceptLocation);
   }, []);
   return { isGetLocation };
 };
