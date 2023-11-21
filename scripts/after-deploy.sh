a6ce0fbedabbb3dd3a644e50e8bc0d0a1e21bbad
sunmin <polarlsm1231@gmail.com>
2023-11-21 20:14:28 +0900

    chore: fix deploy script #3

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -4,6 +4,8 @@ REPOSITORY=/home/ubuntu/picnic-map
 cd $REPOSITORY
 git pull
 cd client
-sudo pm2 kill
-sudo yarn install
-sudo pm2 start yarn --name picnic -- start
+sudo ln -s /root/.nvm/versions/node/v16.15.1/bin/yarn /usr/bin/yarn
+sudo ln -s /root/.nvm/versions/node/v16.15.1/bin/pm2 /usr/bin/pm2
+sudo /usr/bin/pm2 kill
+sudo /usr/bin/yarn
+sudo /usr/bin/pm2 start yarn --name picnic -- start
